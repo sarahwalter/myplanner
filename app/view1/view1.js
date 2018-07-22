c4c64ff1d675591b5eb2cde12352c6ab6533e5b0
longjaso <longjaso@oregonstate.edu>
2018-07-22 15:06:23 -0700

    Debugging

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,9 +11,11 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', ['$http', '$scope', function($http, $scope) {
     $http.get('/users/1', null).then(function(response){
+        console.log(response);
         $scope.users = response.data;
     });
     $http.get('/calendar_events/event/1', null).then(function(response){
+        console.log(response);
         $scope.rules = response.data;
     });
 }]);
