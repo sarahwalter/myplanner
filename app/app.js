ac379127914f91555a3379d00afb4511a703d185
Sarah Walter <waltsara@oregonstate.edu>
2018-08-02 22:33:59 +0300

    calendar and eventsForm are correctly routing

--- app/app.js
+++ app/app.js
@@ -15,6 +15,8 @@ angular.module('myApp', [
   'myApp.logout',
   'myApp.navigation',
   'myApp.paycheck_estimator',
+  'ui.calendar',
+  'myApp.event'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
