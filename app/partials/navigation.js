7ff60c4b247136de6f729561de99a4a1ba9447e4
Tira <tirawickland@gmail.com>
2018-08-10 07:05:20 +0000

    nav bar working

--- app/partials/navigation.js
+++ app/partials/navigation.js
@@ -3,5 +3,7 @@
 angular.module('myApp.navigation', ['ngRoute'])
 .controller('navigationCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
     $scope.isLoggedIn = false;
-    if($rootScope.globals.currentUser){ $scope.isLoggedIn = true}
+    if($rootScope.globals.currentUser){ 
+        console.log("user logged in");
+        $scope.isLoggedIn = true}
 }]);
