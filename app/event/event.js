b37d60f2463f8b70074e5e357474326ba822ef09
longjaso <longjaso@oregonstate.edu>
2018-08-12 21:12:29 -0700

    HTML changes to event form

--- app/event/event.js
+++ app/event/event.js
@@ -2,60 +2,66 @@
 
 angular.module('myApp.event', ['ngRoute', 'ngMaterial'])
 
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/eventForm', {
-            templateUrl: 'event/eventForm.html',
-            controller: 'EventCtrl'
-        });
-    }])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/eventForm', {
+        templateUrl: 'event/eventForm.html',
+        controller: 'EventCtrl'
+    });
+}])
 
-    .controller('EventCtrl', ['$http', '$scope', '$rootScope', '$location', '$window', '$route', function($http, $scope, $rootScope, $location, $window, $route) {
-        if(!$rootScope.globals.currentUser){$location.path('/login')}
-        else{
-            $scope.user_id = $rootScope.globals.currentUser.user_id
-            $scope.eventTitle = "";
-            $scope.eventDate = new Date();
-            $scope.eventTime = new Date(new Date().toDateString() + ' ' + '08:00');
-            $scope.eventNote = "";
-            $scope.eventType = "";
-    
-            $scope.submitEvent = function(){
-                
-                if ($scope.eventTitle == ""){ $scope.errorMessage = "Event title required"}
-                else {
-                var eventSubmit = {
-                    user_id : $rootScope.globals.currentUser.user_id,
-                    start_datetime : datetimeFormatter($scope.eventDate, $scope.eventTime),
-                    end_datetime: null,
-                    title : $scope.eventTitle,
-                    notes : $scope.eventNote,
-                    isFullDay : false,  //this value currently not passed through server to database
-                    rep_stop_date : null,
-                    rep_day_month : null,
-                    rep_day_week : null,
-                    event_type : $scope.eventType,
-                    amount : null,
-                    job_id : null
-                };
-                $http.post('/calendar_events', eventSubmit, null)
-                .then(function successful(response){
-                    /* If successful redirect to calendar page*/
-                    $location.path('/calendar');
-                }, function failure(response){
-                        /* If not successful */
-                        /*server returns error, display message */
-                        $scope.errorMessage = response.data;
-                    });
-                }
+.controller('EventCtrl', ['$http', '$scope', '$rootScope', '$location', '$window', '$route', function($http, $scope, $rootScope, $location, $window, $route) {
+    if(!$rootScope.globals.currentUser){$location.path('/login')}
+    let user_id = $rootScope.globals.currentUser.user_id;
+    $scope.user_id = $rootScope.globals.currentUser.user_id;
+    $scope.eventTitle = "";
+    $scope.eventDate = new Date();
+    $scope.eventTime = new Date(new Date().toDateString() + ' ' + '08:00');
+    $scope.endDate = new Date();
+    $scope.endTime = new Date(new Date().toDateString() + ' ' + '09:00');
+    $scope.eventNote = "";
+    $scope.eventType = "event";
+    $scope.rep_day_week = null;
+    $scope.rep_day_month = null;
+    $scope.rep_stop_date = null;
+    $scope.all_day = false;
+    $scope.amount = 0;
+
+    $scope.submitEvent = function() {
+        if ($scope.eventTitle === "") { $scope.errorMessage = "Event title required"; }
+        if ($scope.rep_day_week === "") { $scope.rep_day_week = null; }
+        if ($scope.rep_day_month === "") { $scope.rep_day_month = null; }
+        else {
+            let eventSubmit = {
+                user_id: user_id,
+                start_datetime: datetimeFormatter($scope.eventDate, $scope.eventTime),
+                end_datetime: null,
+                title: $scope.eventTitle,
+                notes: $scope.eventNote,
+                isFullDay: $scope.all_day,  //this value currently not passed through server to database
+                rep_stop_date: $scope.rep_stop_date,
+                rep_day_month: $scope.rep_day_month,
+                rep_day_week: $scope.rep_day_week,
+                event_type: $scope.eventType,
+                amount: $scope.amount,
+                job_id: null
             };
-    
-            /*Formats JS datetime for MySQL*/
-            function datetimeFormatter(date, time){
-                var newDate = (date.getFullYear()) + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-                var hours = (time.getHours() < 10) ? ("0" + time.getHours()) : time.getHours();
-                var minutes = (time.getMinutes() < 10) ? ("0" + time.getMinutes()) : time.getMinutes();
-                var newTime = hours + ":" + minutes + ":" + "00";
-                return newDate + " " + newTime;
-            }
+            $http.post('/calendar_events', eventSubmit, null).then(function successful(response) {
+                if (response.status !== 201) {
+                    $scope.errorMessage = response.data;
+                } //If unsuccessful server returns error, display message
+                else {
+                    $location.path('/calendar');
+                } // If successful redirect to calendar page
+            });
         }
-    }]);
+    };
+
+    /*Formats JS datetime for MySQL*/
+    function datetimeFormatter(date, time) {
+        let newDate = (date.getFullYear()) + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+        let hours = (time.getHours() < 10) ? ("0" + time.getHours()) : time.getHours();
+        let minutes = (time.getMinutes() < 10) ? ("0" + time.getMinutes()) : time.getMinutes();
+        let newTime = hours + ":" + minutes + ":" + "00";
+        return newDate + " " + newTime;
+    }
+}]);
